95206d107524eb7884c3f7c454b3391c835744b0
Suriya <suryachevrolet@gmail.com>
2025-07-11 13:07:16 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     print(f"The sum of {num1} and {num2} is: {sum_result}") # Expected: The sum of 10 and 5 is: 15
     print("-" * 20)
 
-    # You can add more test cases or different types of code here. SSH to git test. 
+    # You can add more test cases or different types of code here. Branch 2 test. wowie wow.
     print("Repository setup successful! This is your first code.")
